77c53509b374ef0dd776a7863ad18969b32f63c1
webdevaadil <75753195+webdevaadil@users.noreply.github.com>
2022-07-22 11:46:30 +0530

    heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const dotenv = require("dotenv");
+import path from"path"
 dotenv.config({ path: "./config.env" });
 const express = require("express");
 const cookie = require("cookie-parser");
@@ -20,6 +21,23 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/private", require("./routes/private"));
 
 //error handle
+const __dirname = path.resolve();
+
+// --------------------------deployment------------------------------
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+
+// --------------------------deployment------------------------------
 app.use(errorHandler)
 const PORT = process.env.PORT || 5000;
 
